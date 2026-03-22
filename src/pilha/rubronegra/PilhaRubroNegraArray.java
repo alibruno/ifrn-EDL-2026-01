d27b8a2d2b1c770b8453a1d1d49501bfe9681e63
alibruno <alissonbruno318@gmail.com>
2026-03-21 21:41:31 -0300

    feat: PilhaRubroNegraArray - Construtor, increaseCapacity() e reduceCapacity()

--- src/pilha/rubronegra/PilhaRubroNegraArray.java
+++ src/pilha/rubronegra/PilhaRubroNegraArray.java
@@ -4,8 +4,51 @@ import java.util.Arrays;
 
 public class PilhaRubroNegraArray implements PilhaRubroNegra {
     private Object[] data;
-    private int index;
+    private int indexRed;
+    private int indexBlack;
+    private int size;
     private int capacity;
+
+    public PilhaRubroNegraArray(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("capacity must be greater than 0");
+        }
+        this.indexRed = -1;
+        this.indexBlack = capacity;
+        this.size = 0;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public void pushRed(Object o) {
+        if (size == capacity) {
+            increaseCapacity();
+        }
+    }
+
+    @Override
+    public void pushBlack(Object o) {
+        if (size == capacity) {
+            increaseCapacity();
+        }
+
+    }
+
+    @Override
+    public Object popRed() throws PilhaVaziaExcecao {
+        if (size * 3 <= capacity) {
+            reduceCapacity();
+        }
+        return null;
+    }
+
+    @Override
+    public Object popBlack() throws PilhaVaziaExcecao {
+        if (size * 3 <= capacity) {
+            reduceCapacity();
+        }
+        return null;
+    }
 
     @Override
     public Object topRed() throws PilhaVaziaExcecao {
@@ -18,35 +61,24 @@ public class PilhaRubroNegraArray implements PilhaRubroNegra {
     }
 
     @Override
-    public void pushRed(Object o) {
-
-    }
-
-    @Override
-    public void pushBlack(Object o) {
-
-    }
-
-    @Override
-    public Object popRed() throws PilhaVaziaExcecao {
-        return null;
-    }
-
-    @Override
-    public Object popBlack() throws PilhaVaziaExcecao {
-        return null;
-    }
-
-    public boolean isEmpty() {
-        return index == -1;
-    }
-
     public int size() {
-        return index + 1;
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
     }
 
     private void increaseCapacity() {
         Object[] new_data = new Object[capacity *= 2];
+        for (int i = 0; i < data.length; i++)
+            new_data[i] = data[i];
+        data = new_data;
+    }
+
+    private void reduceCapacity() {
+        Object[] new_data = new Object[capacity /= 2];
         for (int i = 0; i < data.length; i++)
             new_data[i] = data[i];
         data = new_data;
