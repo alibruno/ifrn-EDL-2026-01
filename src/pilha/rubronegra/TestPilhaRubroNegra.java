493f86f168464515d3fc767cb9211ada2af94c5f
Patricia <pcbandeira.m@gmail.com>
2026-03-23 19:12:53 +0000

    style: formatação da saída do teste

--- src/pilha/rubronegra/TestPilhaRubroNegra.java
+++ src/pilha/rubronegra/TestPilhaRubroNegra.java
@@ -92,6 +92,9 @@ public class TestPilhaRubroNegra {
         for (int i = 0; i < 5; i++) {
             pilha.pushRed(i);
         }
+
+        System.out.println();
+
         System.out.println(pilha);
 
         System.out.println("tentativa de topBlack()");
@@ -105,6 +108,9 @@ public class TestPilhaRubroNegra {
         for (int i = 9; i > 4; i--) {
             pilha.pushBlack(i);
         }
+
+        System.out.println();
+
         System.out.println(pilha);
         System.out.println("topRed = " + pilha.topRed() + " topBlack = " + pilha.topBlack());
 
@@ -130,6 +136,7 @@ public class TestPilhaRubroNegra {
         }
         System.out.println(pilha);
 
+        System.out.println();
         System.out.println("Remoção pilha vermelha");
         System.out.println("Elemento removido = " + pilha.popRed());
         System.out.println("Topo atual = " + pilha.topRed());
@@ -137,16 +144,17 @@ public class TestPilhaRubroNegra {
         System.out.println("Size All = " + pilha.sizeAll());
         System.out.println(pilha);
 
+        System.out.println();
         System.out.println("Remoção pilha preta");
         System.out.println("Elemento removido = " + pilha.popBlack());
         System.out.println("Topo atual = " + pilha.topBlack());
         System.out.println("Size Black = " + pilha.sizeBlack());
         System.out.println("Size All = " + pilha.sizeAll());
-
         System.out.println(pilha);
 
-        System.out.println("=========================");
-        System.out.println("Teste de reduceCapacity()");
+        System.out.println();
+
+        System.out.println("=============================================== Teste reduceCapacity() ===================================================");
         System.out.println("Remoção de mais um elemento da pilha preta");
         System.out.println("Elemento removido = " + pilha.popBlack());
         System.out.println("Topo atual = " + pilha.topBlack());
@@ -154,8 +162,9 @@ public class TestPilhaRubroNegra {
         System.out.println("Size All = " + pilha.sizeAll());
         System.out.println(pilha);
 
-        System.out.println("REMOÇÃO DE TODOS OS ELEMENTOS VERMELHOS");
+        System.out.println();
 
+        System.out.println("REMOÇÃO DE TODOS OS ELEMENTOS VERMELHOS");
         System.out.println("Elemento removido = " + pilha.popRed());
         System.out.println("Topo atual = " + pilha.topRed());
         System.out.println("Size Red = " + pilha.sizeRed());
@@ -165,6 +174,8 @@ public class TestPilhaRubroNegra {
         System.out.println("Size Red = " + pilha.sizeRed());
         System.out.println("Size All = " + pilha.sizeAll());
         System.out.println(pilha);
+
+        System.out.println();
 
         System.out.println("ADICÃO DE MAIS UM ELEMENTO PARA A PILHA PRETA");
         pilha.pushBlack(9);
@@ -173,6 +184,9 @@ public class TestPilhaRubroNegra {
         System.out.println("Size Black = " + pilha.sizeBlack());
         System.out.println("Size All = " + pilha.sizeAll());
         System.out.println(pilha);
+
+        System.out.println();
+
         System.out.println("REMOÇÃO DE TODOS OS ELEMENTOS PRETOS");
         System.out.println("Elemento removido = " + pilha.popBlack());
         System.out.println("Size Black = " + pilha.sizeBlack());
