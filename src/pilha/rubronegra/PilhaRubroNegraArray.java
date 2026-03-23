5baa6977b6d0af7e4f9379b5a7c98d17034e8a3b
Aguiberto <aguibertocandido@gmail.com>
2026-03-23 16:53:00 +0000

    adicionando comentários

--- src/pilha/rubronegra/PilhaRubroNegraArray.java
+++ src/pilha/rubronegra/PilhaRubroNegraArray.java
@@ -3,6 +3,8 @@ package pilha.rubronegra;
 import java.util.Arrays;
 
 public class PilhaRubroNegraArray implements PilhaRubroNegra {
+
+    //Atributos da classe
     private Object[] data;
     private int redIndex;
     private int blackIndex;
@@ -10,7 +12,10 @@ public class PilhaRubroNegraArray implements PilhaRubroNegra {
     private int blackSize;
     private int capacity;
 
+    //Construtor
     public PilhaRubroNegraArray(int capacity) {
+
+        // Se a pilha não pode ter uma capacidade menor que 0
         if (capacity <= 0) {
             throw new IllegalArgumentException("capacity must be greater than 0");
         }
@@ -23,24 +28,40 @@ public class PilhaRubroNegraArray implements PilhaRubroNegra {
     }
 
     @Override
+    // Método para adicionar um objeto na pilha vermelha
     public void pushRed(Object o) {
+
+        // Verifica se a pilha está cheia
+        // Se estiver aumenta a capacidade do array
         if (redSize + blackSize == capacity) {
             increaseCapacity();
         }
+
+        //Adiciona o objeto na pilha vermelha
         data[++redIndex] = o;
         redSize++;
     }
 
     @Override
+    // Método para adiconar um objeto na pilha preta
     public void pushBlack(Object o) {
+
+        // Verifica se a pilha está cheia
+        // Se estiver cheia aumenta sua capacidade
         if (redSize + blackSize == capacity) {
             increaseCapacity();
         }
+
+        // Adiciona um objeto a pilha preta
+        /*A pilha preta tem adição de objetos da esqueda para a direita
+        Por isso a adicição é feita decrementando o índice do array */
         data[--blackIndex] = o;
         blackSize++;
     }
 
     @Override
+    // Método para remoção de um objeto da pilha vermelha
+    // A remoção ocorre no topo da pilha
     public Object popRed() throws PilhaVaziaExcecao {
         if (isEmptyRed()) {
             throw new PilhaVaziaExcecao("Red Stack is empty");
