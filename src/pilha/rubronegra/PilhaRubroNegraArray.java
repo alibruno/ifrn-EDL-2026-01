3ddb400cedd0d7abe00adfa4d0030e1d78b35b7f
Aguiberto <aguibertocandido@gmail.com>
2026-03-23 17:32:53 +0000

    comentários completos

--- src/pilha/rubronegra/PilhaRubroNegraArray.java
+++ src/pilha/rubronegra/PilhaRubroNegraArray.java
@@ -62,10 +62,12 @@ public class PilhaRubroNegraArray implements PilhaRubroNegra {
     @Override
     // Método para remoção de um objeto da pilha vermelha
     // A remoção ocorre no topo da pilha
+    // Esse método pode gerar uma excessão, visto que não é possível remover elementos de uma pilha vazia
     public Object popRed() throws PilhaVaziaExcecao {
         if (isEmptyRed()) {
             throw new PilhaVaziaExcecao("Red Stack is empty");
         }
+        // verifica se o tamanho da pilha chegou em 1/3 do seu total
         if (((redSize - 1) + blackSize) * 3 <= capacity) {
             reduceCapacity();
         }
@@ -74,10 +76,16 @@ public class PilhaRubroNegraArray implements PilhaRubroNegra {
     }
 
     @Override
+    // Método para a remoção de um objeto da pilha preta
+    // A remoção ocorre no topo da pilha (da esqueda para a direita)
+    // Esse método pode gerar uma excessão, visto que não é possível remover elementos de uma pilha vazia
     public Object popBlack() throws PilhaVaziaExcecao {
+
         if (isEmptyBlack()) {
             throw new PilhaVaziaExcecao("Black Stack is empty");
         }
+
+        // verifica se o tamanho da pilha chegou em 1/3 do seu total
         if ((redSize + (blackSize - 1)) * 3 <= capacity) {
             reduceCapacity();
         }
@@ -86,6 +94,8 @@ public class PilhaRubroNegraArray implements PilhaRubroNegra {
     }
 
     @Override
+    // Mostra o elemento no topo da pilha VERMELHA
+    // Esse método pode gerar uma excessão caso não haja elementos na pilha
     public Object topRed() throws PilhaVaziaExcecao {
         if (isEmptyRed()) {
             throw new PilhaVaziaExcecao("Red Stack is empty");
@@ -94,6 +104,8 @@ public class PilhaRubroNegraArray implements PilhaRubroNegra {
     }
 
     @Override
+    // Mostra o elemento no topo da pilha PRETA
+    // Esse método pode gerar uma excessão caso não haja elementos na pilha
     public Object topBlack() throws PilhaVaziaExcecao {
         if (isEmptyBlack()) {
             throw new PilhaVaziaExcecao("Black Stack is empty");
@@ -102,45 +114,56 @@ public class PilhaRubroNegraArray implements PilhaRubroNegra {
     }
 
     @Override
+    // Retorna o tamanho completo da pilha
     public int sizeAll() {
         return redSize + blackSize;
     }
 
     @Override
+    // Retorna o tamanho da pilha vermelha
     public int sizeRed() {
         return redSize;
     }
 
     @Override
+    // Retorna o tamanho da pilha preta
     public int sizeBlack() {
         return blackSize;
     }
 
     @Override
+    // Verifica se o array completo está vazio
     public boolean isEmptyAll() {
         return redSize == 0 && blackSize == 0;
     }
 
     @Override
+    // Verifica se a pilha VERMELHA está vazia
     public boolean isEmptyRed() {
         return sizeRed() == 0;
     }
 
     @Override
+    // Verifica se a pilha PRETA está vazia
     public boolean isEmptyBlack() {
         return sizeBlack() == 0;
     }
 
+    // Método que aumente a capacidade do array
+    // Usa a estratégia de crescimento de DUPLICAÇÃO
     private void increaseCapacity() {
         Object[] new_data = new Object[capacity *= 2];
         this.data = copyRedAndBlackStacksElementsNewArray(this.data, new_data);
     }
 
+    // Método de diminui a capacidade caso o tamanho de ambas as pilhas somadas sejam menou ou igual a 1/3
     private void reduceCapacity() {
         Object[] new_data = new Object[capacity /= 2];
         this.data = copyRedAndBlackStacksElementsNewArray(this.data, new_data);
     }
-
+    // Método auxiliar para o increaseCapacity e o reduceCapacity
+    // Cria um novo array e copia os valores do array antigo
+    // Retorna o novo array com ca capacidade modificada e com os antigos elementos posicionados nos seus lugares
     private Object[] copyRedAndBlackStacksElementsNewArray(Object[] data, Object[] new_data) {
         // Copy elements from the red stack
         for (int i = 0; i < redSize; i++) {
